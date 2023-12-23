3cb59aff9d8624d064c36ddfb8e59cc464f62878
alaltru1992 <33177697+alaltru1992@users.noreply.github.com>
2023-12-23 14:03:55 +0300

    webpack index fix

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,7 +5,7 @@ import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
 
 export default (env: buildEnv) =>{
     const paths: buildPaths = {
-        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
         output: path.resolve(__dirname, 'build'),
         html: path.resolve(__dirname, 'public', 'index.html'),
         src: path.resolve(__dirname, 'src'),
